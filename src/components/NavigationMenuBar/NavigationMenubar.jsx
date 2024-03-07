07b8b65e5b4ecb8a32ff2ece1bfa597faaf5cd38
marsrian <marsrian40@gmail.com>
2024-03-07 12:37:31 +0600

    deploy vercel

--- src/components/NavigationMenuBar/NavigationMenubar.jsx
+++ src/components/NavigationMenuBar/NavigationMenubar.jsx
@@ -24,7 +24,7 @@ const NavigationMenubar = () => {
 
   return (
     <div className="bg-slate-600 w-full">
-      <div className="flex justify-between items-center gap-5 max-w-screen-2xl px-7 md:px-10 mx-auto">
+      <div className="flex justify-between items-center gap-5 max-w-screen-2xl px-7 md:px-10 mx-auto py-3 text-white">
         <Link href={"/"}>Try Pass</Link>
         <div className="flex items-center gap-2 uppercase">
           {!session ? (
